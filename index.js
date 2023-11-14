aac1c09cec17dfff90e7acf834f51a321109808c
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2023-11-14 17:05:23 +0530

    Portal tags and modal task completed

--- index.js
+++ index.js
@@ -1,32 +1,82 @@
+const tag1 = "Tag 1";
+const tag2 = "Tag 2";
+const tag3 = "Tag 3";
+const tag4 = "Tag 4";
+const tag5 = "Tag 5";
+const tag6 = "Tag 6";
+const tag7 = "Tag 7";
+const tag8 = "Tag 8";
+let tags = [tag1, tag2, tag3, tag4, tag5, tag6, tag7, tag8, tag4, tag5, tag6, tag7, tag8,tag1, tag2, tag3, tag4, tag5, tag6, tag7, tag8, tag4, tag5, tag6, tag7, tag8];
 const seeMoreBtn = document.getElementById("seeMoreBtn");
 const addTagBtn = document.getElementById("addTagBtn");
 const tagDropdown = document.getElementById("tagDropdown");
 const dropdownContainer = document.getElementById("dropdownContainer");
-let hiddenButtons = document.querySelectorAll(".hidden");
+
+
+// function showTags() {
+//     const buttonContainer = document.getElementsByClassName("buttonContainer")[0];
+//     tags.forEach((text, index) => {
+//         const button = document.createElement('button');
+//         button.textContent = text;
+//         buttonContainer.append(button);
+//     });
+// }
 
 function showTags() {
-    const buttons = document.querySelectorAll('.hiddenElement');
-    console.log(buttons)
-    buttons.forEach(button => {
-        button.classList.toggle('hidden');
-    });
+    const buttonContainer = document.querySelector(".buttonContainer");
+
+    // Check if the container element exists
+    if (buttonContainer) {
+        tags.forEach((text, index) => {
+            const button = document.createElement('button');
+            button.textContent = text;
+            buttonContainer.append(button);
+        });
+    } else {
+        console.error('Element with class "buttonContainer" not found.');
+    }
 }
 
-showTags();
+
+// showTags();
+
 // See More
 
 seeMoreBtn.addEventListener("click", function () {
     if (seeMoreBtn.innerText == "See more") {
+        showPrompt()
         seeMoreBtn.innerText = "See less";
-    } else {
+    } else if (seeMoreBtn.innerText == "See less") {
+        const modal = document.getElementsByClassName("modal")[0];
+        document.body.removeChild(modal);
         seeMoreBtn.innerText = "See more";
     }
-    showTags();
+    // showTags();
 });
 
 // Show Prompt
 
 function showPrompt() {
+    const buttonContainer = document.createElement('div');
+    buttonContainer.classList.add("buttonContainer");
+    Object.assign(buttonContainer.style, {
+        display: 'flex',
+        flexWrap: "wrap",
+        justifyContent: "space-around",
+        alignItems: "center",
+        height: "200px",
+        width: "335px",
+        overflowY: "scroll"
+    });
+    buttonContainer.innerHTML = '';
+
+    // Loop through the array and create buttons
+    tags.forEach((text, index) => {
+        const button = document.createElement('button');
+        button.textContent = text;
+        buttonContainer.appendChild(button);
+    });
+
     const modal = document.createElement('div');
     modal.className = 'modal';
 
@@ -42,6 +92,7 @@ function showPrompt() {
     closeButton.textContent = '✕';
     closeButton.onclick = () => {
         document.body.removeChild(modal);
+        seeMoreBtn.innerText = "See more";
     };
 
     const input = document.createElement('input');
@@ -54,22 +105,19 @@ function showPrompt() {
     addButton.onclick = () => {
         const text = input.value;
         if (text.trim() !== '') {
-            const output = document.getElementById('buttonSets');
-            const newElement = document.createElement('button');
-            newElement.classList.add("hiddenElement");
-            if (seeMoreBtn.innerText !== "See less") 
-                newElement.classList.add("hidden");
-            newElement.textContent = text;
-            output.appendChild(newElement);
+            tags.push(text);
+            showTags();
+            console.log(tags)
             input.value = "";
         }
     };
-    
 
     modal.appendChild(closeButton);
+    modal.appendChild(buttonContainer);
     modal.appendChild(input);
     modal.appendChild(addButton);
     document.body.appendChild(modal);
+    seeMoreBtn.innerText = "See less";
 }
 
 // Add Tag
